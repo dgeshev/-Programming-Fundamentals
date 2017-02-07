4d2b8630020eb0f877859434970237fc3fc16746
dgeshev <geshev.doncho@gmail.com>
2017-02-07 22:45:30 +0200

    debugging exercises added.

---
+++ 05.Debugging-Exercises/01.Tricky Strings/Tricky Strings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class TrickyStrings
+{
+    public static void Main(string[] args)
+    {
+        var delimiter = Console.ReadLine();
+        var numberOfStrings = int.Parse(Console.ReadLine());
+        var result = string.Empty;
+        var currentString = string.Empty;
+
+        for (int i = 0; i < numberOfStrings; i++)
+        {
+            currentString += Console.ReadLine();
+            currentString += delimiter;
+        }
+        result = currentString.Remove(currentString.Length - 1);
+        Console.WriteLine(result);
+    }
+}
