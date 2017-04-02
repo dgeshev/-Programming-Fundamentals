feda60d11341baa0811d9a7f47f9ce6ddcfb021f
dgeshev <geshev.doncho@gmail.com>
2017-04-02 23:25:25 +0300

    Lambda and Linq more exercises added.

---
+++ Extended mode/11.LambdaAndLINQMoreExercises/01.LambadaExpressions/LambadaExpressions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.LambadaExpressions
+{
+    public class LambadaExpressions
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+            var resultDict = new Dictionary<string, string>();
+
+            while (input != "lambada")
+            {
+                var toknes = input.Split(new[] { ' ', '=', '>', }, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+                if (input != "dance")
+                {
+                    var leftPart = toknes[0];
+                    var rightPart = toknes[1];
+
+                    if (!resultDict.ContainsKey(leftPart))
+                    {
+                        resultDict[leftPart] = rightPart;
+                    }
+
+                    resultDict[leftPart] = rightPart;
+                }
+
+                else
+                {
+                    resultDict = resultDict.ToDictionary(x => x.Key, x => x.Key + '.' + x.Value);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var entry in resultDict)
+            {
+                Console.WriteLine($"{entry.Key} => {entry.Value}");
+
+            }
+
+        }
+    }
+}
---
+++ Extended mode/11.LambdaAndLINQMoreExercises/02.OrderedBankingSystem/OrderedBankingSystem.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.OrderedBankingSystem
+{
+    public class OrderedBankingSystem
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+            var resultDict = new Dictionary<string, Dictionary<string, decimal>>();
+
+            while (input != "end")
+            {
+                var toknes = input.Split(new[] { ' ', '-', '>' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+                var bank = toknes[0];
+                var account = toknes[1];
+                var balance = decimal.Parse(toknes[2]);
+
+                if (!resultDict.ContainsKey(bank))
+                {
+                    resultDict[bank] = new Dictionary<string, decimal>();
+                }
+
+                if (!resultDict[bank].ContainsKey(account))
+                {
+                    resultDict[bank][account] = 0;
+                }
+
+                resultDict[bank][account] += balance;
+               
+
+                input = Console.ReadLine();          
+            }
+          
+            foreach (var bank in resultDict.OrderByDescending(bank => bank.Value.Sum(account => account.Value))
+                .ThenByDescending(bank => bank.Value.Max(account => account.Value)))
+            {
+                foreach (var account in bank.Value.OrderByDescending(account => account.Value))
+                {
+                    Console.WriteLine($"{account.Key} -> {account.Value} ({bank.Key})");
+                }
+            } 
+        }
+    }
+}
