2ce36b901959457baab49faaea193b2b57eb694f
dgeshev <geshev.doncho@gmail.com>
2017-04-07 15:25:33 +0300

    More exercises added.

---
+++ Extended mode/14.Strings-More-Exercises/01.SerializeString/SerializeString.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01.SerializeString
+{
+    public class SerializeString
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+            var result = new Dictionary<char, List<int>>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+
+                if (!result.ContainsKey(input[i]))
+                {
+                    result[input[i]] = new List<int>();                   
+                }
+
+                result[input[i]].Add(i);
+            }
+
+            foreach (var kvp in result)
+            {
+                Console.WriteLine($"{kvp.Key}:{string.Join("/", kvp.Value)}");
+            }
+        }
+    }
+}
---
+++ Extended mode/14.Strings-More-Exercises/02.Stateless/Stateless.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.Stateless
+{
+    public class Stateless
+    {
+        public static void Main()
+        {
+            var state = Console.ReadLine();
+            var fiction = Console.ReadLine();
+
+            while (state != "collapse")
+            {
+                var tokens = state.Split(new[] { ' ' },
+                    StringSplitOptions.RemoveEmptyEntries)
+                    .ToList();
+
+                while (fiction.Length != 0)
+                {
+                    for (int i = 0; i < tokens.Count; i++)
+                    {
+                        if (tokens[i].Contains(fiction))
+                        {
+                            tokens[i] = tokens[i].Replace(fiction, string.Empty);
+                        }
+                    }
+
+                    if (fiction.Length >= 2)
+                    {
+                        fiction = fiction.Substring(1, fiction.Length - 2);
+                    }
+
+                    else
+                    {
+                        fiction = fiction.Substring(1);
+                    }
+                }
+
+                if (tokens.Count == 1 && tokens.Contains(""))
+                {
+                    Console.WriteLine("(void)");
+
+                }
+                else
+                {
+                    Console.WriteLine(string.Join(" ", tokens).Trim());
+                }
+
+                state = Console.ReadLine();
+
+                if (state == "collapse")
+                {
+                    break;
+                }
+
+                fiction = Console.ReadLine();
+
+            }
+        }
+    }
+}
